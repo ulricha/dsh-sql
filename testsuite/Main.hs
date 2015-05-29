b22bf82d9ac6d0b2e2d82577a5f2d020542c8bdb
Alexander Ulrich <alex@etc-network.de>
2015-05-29 22:15:39 +0200

    Testsuite: take database name as parameter.

--- testsuite/Main.hs
+++ testsuite/Main.hs
@@ -1,21 +1,22 @@
 module Main where
 
 import System.Environment
+import Text.Printf
 
 import Database.HDBC.ODBC
 
 import Database.DSH.Tests
 import Database.DSH.Backend.Sql
 
-getConn :: String -> IO Connection
-getConn connString = connectODBC connString
+getConn :: String -> IO SqlBackend
+getConn connString = sqlBackend <$> connectODBC connString
 
 main :: IO ()
 main = do
     argv <- getArgs
     case argv of
-        [connString] -> do
-            c <- sqlBackend <$> getConn connString
+        [db] -> do
+            c <- getConn (printf "DSN=%s" db)
             runTests c defaultTests
         _            ->
-            error "usage: sqltests <connection string>"
+            error "usage: sqltests <odbc dbname>"
