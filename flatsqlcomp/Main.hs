ed63b789ae3a2312b8edb26122bbef84d8252777
Alexander Ulrich <alex@etc-network.de>
2016-01-18 11:15:26 +0100

    flatsqlcomp: Add pretty-printing mode.

--- flatsqlcomp/Main.hs
+++ flatsqlcomp/Main.hs
@@ -2,26 +2,60 @@
 -- shape structure to stdout.
 module Main where
 
+import           System.Console.GetOpt
 import           System.Environment
+import           System.Exit
 import           System.IO
 
 import           Data.Aeson
-import qualified Data.ByteString.Lazy     as BL
+import qualified Data.ByteString.Lazy             as BL
 
-import           Database.DSH.CL.Parser
-import           Database.DSH.Common.QueryPlan
 import           Database.DSH.Backend.Sql.CodeGen
+import           Database.DSH.CL.Parser
+import           Database.DSH.Common.Pretty
+import           Database.DSH.Common.QueryPlan
 
-readInput :: [String] -> IO String
-readInput ["-i", inFile] = withFile inFile ReadMode hGetContents
-readInput _              = getContents
+data Options = Options { optInput :: IO String
+                       , optPrint :: Bool
+                       }
 
-writeQueries :: Shape SqlCode -> IO ()
-writeQueries shape = BL.hPut stdout (encode shape)
+startOptions :: Options
+startOptions = Options { optInput  = getContents
+                       , optPrint  = False
+                       }
+
+options :: [OptDescr (Options -> IO Options)]
+options =
+    [ Option "i" ["input"]
+          (ReqArg (\arg opt -> return opt { optInput = readFile arg })
+           "FILE")
+            "Input file"
+    , Option "p" ["print"]
+          (NoArg (\opt -> return opt { optPrint = True }))
+           "Just pretty-print the input (no compilation)"
+    , Option "h" ["help"]
+          (NoArg
+           (\_ -> do
+                prg <- getProgName
+                hPutStrLn stderr (usageInfo prg options)
+                exitSuccess))
+           "Show help"
+    ]
+
+printQueries :: Shape SqlCode -> IO ()
+printQueries shape = BL.hPut stdout (encode shape)
 
 main :: IO ()
 main = do
-    input <- getArgs >>= readInput
+    args  <- getArgs
+
+    let (actions, _, _) = getOpt RequireOrder options args
+    opts  <- foldl (>>=) (return startOptions) actions
+    input <- optInput opts
     case parseCL input of
         Left msg -> error msg
-        Right cl -> writeQueries (comprehensionCodeGen cl)
+        Right cl ->
+            if optPrint opts
+                then putStrLn (pp cl) >> exitSuccess
+                else printQueries (comprehensionCodeGen cl)
+
