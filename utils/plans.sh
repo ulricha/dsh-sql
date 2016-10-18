83faf81f4180c065abb0a736b6113a0023cbd866
Alexander Ulrich <alex@etc-network.de>
2016-10-18 15:18:54 +0200

    Delete unused helper scripts

--- utils/plans.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-${PWD}/.cabal-sandbox/bin/vldot -i ${1}_vl.plan | dot -Tpdf -o ${1}_vl.pdf
-${PWD}/.cabal-sandbox/bin/vldot -i ${1}_vl_opt.plan | dot -Tpdf -o ${1}_vl_opt.pdf
-${PWD}/.cabal-sandbox/bin/tadot -i ${1}_ta.plan | dot -Tpdf -o ${1}_ta.pdf
-${PWD}/.cabal-sandbox/bin/tadot -i ${1}_opt_ta.plan | dot -Tpdf -o ${1}_opt_ta.pdf
